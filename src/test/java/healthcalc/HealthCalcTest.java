d56f120eb7b92c096a6bf299368c78f84466ab2c
ainhoasb <160436409+ainhoasb@users.noreply.github.com>
2024-03-12 21:58:43 +0100

    Update HealthCalcTest.java

--- src/test/java/healthcalc/HealthCalcTest.java
+++ src/test/java/healthcalc/HealthCalcTest.java
@@ -1,11 +1,8 @@
 package healthcalc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
