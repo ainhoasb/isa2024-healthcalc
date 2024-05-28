b48fe0b0e1e1ac5bbfa21f13285ce62de0236b9b
Ainhoa <ainersb@gmail.com>
2024-05-28 11:56:42 +0200

    Modificación en HealthCalc y HealthCalcImpl para usar el Enumerado Gender

--- src/main/java/healthcalc/HealthCalcImpl.java
+++ src/main/java/healthcalc/HealthCalcImpl.java
@@ -17,17 +17,17 @@ public class HealthCalcImpl implements HealthCalc {
     }
 
     @Override
-    public float idealWeight(int height, char gender) throws Exception{
+    public float idealWeight(int height, Gender gender) throws Exception{
         
         if(height<140 || height>250){
             throw new Exception("Altura incorrecta, debe estar entre 140cm y 250cm");
-        }else if(gender != 'm' && gender != 'M' && gender != 'w' && gender != 'W'){
-            throw new Exception("Género incorrecto, debe ser 'm' o 'w'");
+        }else if(gender != Gender.FEMALE && gender != Gender.MALE){
+            throw new Exception("Género incorrecto, debe ser FEMALE o MALE ");
         }
 
         float idealWeight;
         
-        if (gender == 'm' || gender == 'M'){
+        if (gender == Gender.MALE){
             idealWeight = height - 100 - ((height - 150) / 4);
         }else{
             idealWeight = height - 100 - ((height - 150) / 2.5f);
@@ -37,21 +37,21 @@ public class HealthCalcImpl implements HealthCalc {
     }
 
     @Override
-    public float basalMetabolicRate(float weight, int height, char gender, int age) throws Exception{
+    public float basalMetabolicRate(float weight, int height, Gender gender, int age) throws Exception{
 
         if(weight<=0){
             throw new Exception("Peso incorrecto, debe ser mayor que 0");
         }else if(height<140 || height>250){
             throw new Exception("Altura incorrecta, debe estar entre 140cm y 250cm");
-        }else if(gender != 'm' && gender != 'M' && gender != 'w' && gender != 'W'){
-            throw new Exception("Género incorrecto, debe ser 'm' o 'w'");
+        }else if(gender != Gender.FEMALE && gender != Gender.MALE){
+            throw new Exception("Género incorrecto, debe ser FEMALE o MALE");
         }else if(age<0 || age>120){
             throw new Exception("Edad incorrecta, debe ser mayor que 0");
         }
 
         float basalMetabolicRate;
 
-        if (gender == 'm' || gender == 'M'){
+        if (gender == Gender.MALE){
             basalMetabolicRate = 10 * weight + 6.25f * height - 5 * age + 5;
         }else{
             basalMetabolicRate = 10 * weight + 6.25f * height - 5 * age - 161;
